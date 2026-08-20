87146a29c0f93d7231af9fd1a0dc44121791ea95
m1nk12 <minhnguyendang3@gmail.com>
2026-08-21 00:20:47 +0700

    fix: remove duplicate record when transform from bronze to silver

--- dbt/f1_data_warehouse/models/staging/stg_races.sql
+++ dbt/f1_data_warehouse/models/staging/stg_races.sql
@@ -1,7 +1,26 @@
+with source as (
+    select * from {{ source('bronze', "races") }}
+),
+
+deduplicate as (
+    select * from(
+        select
+            *,
+            row_number() over(
+                partition by season, round
+                order by time_stamp desc
+            ) as rn
+        from source
+    ) x
+    where rn = 1
+)
+
+
+
 SELECT
     season,
     round,
     name,
     date,
     time
-from {{ source('bronze', 'races') }}
+from deduplicate
